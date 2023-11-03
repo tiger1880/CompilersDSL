adcf931bfa54b19d23b424c04998696c51b98029
tejalkul <tejalk00@gmail.com>
2023-11-03 23:24:10 +0530

    update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,6 +1,18 @@
 #include "symbol_table.hpp"
 #include<map>
+#include<string>
+#include<vector>
+
+using namespace std;
 
 map<string, STentry> SymTab;
 
 // Define functions here
+
+void insertType(char* name, enum type t,enum eletype etype) {
+    SymTab[name].Type = t;
+    SymTab[name].Eletype = etype;
+}
+
+
+

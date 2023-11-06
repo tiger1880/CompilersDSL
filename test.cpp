a715efc4599ec8defd3f36d86431cc45486364f1
Muskan Jaiswal <cs21btech11037@iith.ac.in>
2023-11-06 11:09:10 +0530

    update logical grammar

--- test.cpp
+++ test.cpp
@@ -12,6 +12,6 @@ int main(){
    cout<<x;
    return 0;
 }
-// {if(!(arithCompatible($1) && arithCompatible($3))) semanticError("Error: Semantic error incompatible datatype"); $$ = BOOL;  }
-//             {if(!(arithCompatible($1) && arithCompatible($3))) semanticError("Error: Semantic error incompatible datatype"); $$ = BOOL;  }
-// // {if(!(arithCompatible($2))) semanticError("Error: Semantic error incompatible datatype");   }
+
+            // {if(!(arithCompatible($1) && arithCompatible($3))) semanticError("Error: Semantic error incompatible datatype"); $$ = BOOL;  }
+// {if(!(arithCompatible($2))) semanticError("Error: Semantic error incompatible datatype");   }
